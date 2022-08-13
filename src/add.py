e1bfe0fbc3a0572ec84cbe8e48dc42face6a4972
Joaquin Gianantonio <joaquin.gianantonio@gmail.com>
2022-08-13 20:18:43 -0300

    add src directory and add.py

---
+++ src/add.py
@@ -0,0 +1,13 @@
+def add(x:init, y:int=100) -> int:
+    """
+    Add two numbers together.
+
+    Args: 
+        x (int): The first number.
+        y (int): The second number. Defaults to 100.
+    Returns:
+        int: the sum of the two numbers.
+
+    """
+
+    return x + y
